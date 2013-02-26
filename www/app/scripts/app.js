552408aaf48d476739378d48a6b26daaa53037e8
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-02-26 16:00:24 +0100

    modified www app

--- www/app/scripts/app.js
+++ www/app/scripts/app.js
@@ -1,17 +1,20 @@
 'use strict';
 
 var wwwApp = angular.module('wwwApp', [])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/find', {
-        templateUrl: 'views/main.html',
-        controller: 'SearchCtrl'
-      }) 
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+.config(['$routeProvider','$locationProvider', 
+        function($routeProvider, $locationProvider) {
+            $locationProvider.html5mode(true)
+                .hashPrefix('!');
+            $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/find', {
+                templateUrl: 'views/main.html',
+                controller: 'SearchCtrl'
+            }) 
+            .otherwise({
+                redirectTo: '/'
+            });
+        }]);
